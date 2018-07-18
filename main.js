41ad9bc32f867acb215ea8d7b25ab1f0c220e3ad
Yuni-Q <carnotcycle2@naver.com>
2018-07-18 18:46:17 +0900

    node-mysql complete

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var http = require('http');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
-var db = require('./lib/db')
-var topic = require('./lib/topic')
-var author = require('./lib/author')
+var db = require('./lib/db');
+var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -30,6 +30,12 @@ var app = http.createServer(function(request,response){
       author.home(request, response);
     } else if(pathname === '/author/create_process'){
       author.create_process(request, response);
+    } else if(pathname === '/author/update'){
+      author.update(request, response);
+    } else if(pathname === '/author/update_process'){
+      author.update_process(request, response);
+    } else if(pathname === '/author/delete_process'){
+      author.delete_process(request, response);
     } else {
       
       response.writeHead(404);
